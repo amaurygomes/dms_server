327725068c6c07594e31eb5f40a1c53041c78330
Amaury Gomes Ibanez <amaury_mju@hotmail.com>
2025-12-04 00:18:09 -0400

    Revert "feat: Adiciona containers docker para o Elixir e Postgres"

--- apps/dms_repo/lib/dms_repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule DmsRepo do
-  use Ecto.Repo,
-    otp_app: :dms_repo,
-    adapter: Ecto.Adapters.Postgres
-end
--- apps/dms_repo/test/dms_repo_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule DmsRepoTest do
-  use ExUnit.Case
-  doctest DmsRepo
-
-  test "greets the world" do
-    assert DmsRepo.hello() == :world
-  end
-end
